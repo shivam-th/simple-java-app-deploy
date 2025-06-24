646f7e6a4c7fa1f71d4a557c8af6336424c2ea26
shivam <shivamthakur0567@gmail.com>
2025-06-24 22:46:02 +0530

    Test18

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
